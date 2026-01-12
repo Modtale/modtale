65ad82ddde15c6877517dbc3acf63dd1989d226c
Villagers654 <110007851+Villagers654@users.noreply.github.com>
2026-01-11 21:09:01 -0500

    Log the source of a download

--- backend/src/main/java/net/modtale/model/analytics/ProjectAnalyticsDetail.java
+++ backend/src/main/java/net/modtale/model/analytics/ProjectAnalyticsDetail.java
@@ -9,39 +9,39 @@ public class ProjectAnalyticsDetail {
     private long totalDownloads;
     private long totalViews;
 
-    private Map<String, List<AnalyticsDataPoint>> versionDownloads;
+    private long totalApiDownloads;
+    private long totalFrontendDownloads;
+
     private List<AnalyticsDataPoint> views;
+    private Map<String, List<AnalyticsDataPoint>> versionDownloads;
     private List<AnalyticsDataPoint> ratingHistory;
 
     public ProjectAnalyticsDetail() {}
 
-    public ProjectAnalyticsDetail(String projectId, String projectTitle,
-                                  long totalDownloads, long totalViews,
-                                  Map<String, List<AnalyticsDataPoint>> versionDownloads,
-                                  List<AnalyticsDataPoint> views,
-                                  List<AnalyticsDataPoint> ratingHistory) {
-        this.projectId = projectId;
-        this.projectTitle = projectTitle;
-        this.totalDownloads = totalDownloads;
-        this.totalViews = totalViews;
-        this.versionDownloads = versionDownloads;
-        this.views = views;
-        this.ratingHistory = ratingHistory;
-    }
-
     public String getProjectId() { return projectId; }
-    public String getProjectTitle() { return projectTitle; }
-    public long getTotalDownloads() { return totalDownloads; }
-    public long getTotalViews() { return totalViews; }
-    public Map<String, List<AnalyticsDataPoint>> getVersionDownloads() { return versionDownloads; }
-    public List<AnalyticsDataPoint> getViews() { return views; }
-    public List<AnalyticsDataPoint> getRatingHistory() { return ratingHistory; }
-
     public void setProjectId(String projectId) { this.projectId = projectId; }
+
+    public String getProjectTitle() { return projectTitle; }
     public void setProjectTitle(String projectTitle) { this.projectTitle = projectTitle; }
+
+    public long getTotalDownloads() { return totalDownloads; }
     public void setTotalDownloads(long totalDownloads) { this.totalDownloads = totalDownloads; }
+
+    public long getTotalViews() { return totalViews; }
     public void setTotalViews(long totalViews) { this.totalViews = totalViews; }
-    public void setVersionDownloads(Map<String, List<AnalyticsDataPoint>> versionDownloads) { this.versionDownloads = versionDownloads; }
+
+    public long getTotalApiDownloads() { return totalApiDownloads; }
+    public void setTotalApiDownloads(long totalApiDownloads) { this.totalApiDownloads = totalApiDownloads; }
+
+    public long getTotalFrontendDownloads() { return totalFrontendDownloads; }
+    public void setTotalFrontendDownloads(long totalFrontendDownloads) { this.totalFrontendDownloads = totalFrontendDownloads; }
+
+    public List<AnalyticsDataPoint> getViews() { return views; }
     public void setViews(List<AnalyticsDataPoint> views) { this.views = views; }
+
+    public Map<String, List<AnalyticsDataPoint>> getVersionDownloads() { return versionDownloads; }
+    public void setVersionDownloads(Map<String, List<AnalyticsDataPoint>> versionDownloads) { this.versionDownloads = versionDownloads; }
+
+    public List<AnalyticsDataPoint> getRatingHistory() { return ratingHistory; }
     public void setRatingHistory(List<AnalyticsDataPoint> ratingHistory) { this.ratingHistory = ratingHistory; }
 }
