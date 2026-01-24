b43b468d861b2ff0c23a6f38f55f41c4b4af78aa
Villagers654 <110007851+Villagers654@users.noreply.github.com>
2026-01-24 15:11:29 -0500

    Support for latest game version

--- backend/src/main/java/net/modtale/service/resources/ModService.java
+++ backend/src/main/java/net/modtale/service/resources/ModService.java
@@ -71,7 +71,7 @@ public class ModService {
     );
 
     private static final Set<String> ALLOWED_GAME_VERSIONS = Set.of(
-            "2026.01.13-dcad8778f", "2026.01.17-4b0f30090"
+            "2026.01.13-dcad8778f", "2026.01.17-4b0f30090", "2026.01.24-6e2d4fc36"
     );
 
     private static final Set<String> ALLOWED_CLASSIFICATIONS = Set.of(
