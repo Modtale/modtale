4367fe74d852887c5e63ccb28aa828722d900057
Villagers654 <110007851+Villagers654@users.noreply.github.com>
2026-02-13 22:26:08 -0500

    Fix deploy

--- backend/src/main/java/net/modtale/repository/resources/ReportRepository.java
+++ backend/src/main/java/net/modtale/repository/resources/ReportRepository.java
@@ -2,9 +2,12 @@ package net.modtale.repository.resources;
 
 import net.modtale.model.resources.Report;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import java.util.List;
 
 public interface ReportRepository extends MongoRepository<Report, String> {
     List<Report> findByStatus(Report.ReportStatus status);
+
+    @Query("{ 'targetId': ?0, 'targetType': 'PROJECT' }")
     List<Report> findByProjectId(String projectId);
 }
