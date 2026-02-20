d4fb2ff6a7e7150cf79f92fa6f950e0236709b7c
Villagers654 <110007851+Villagers654@users.noreply.github.com>
2026-02-20 03:51:11 -0500

    Very initial modjam work

--- backend/src/main/java/net/modtale/config/security/SecurityConfig.java
+++ backend/src/main/java/net/modtale/config/security/SecurityConfig.java
@@ -218,9 +218,7 @@ public class SecurityConfig {
                         .requestMatchers("/sitemap.xml", "/actuator/health").permitAll()
                         .requestMatchers("/client-metadata.json").permitAll()
                         .requestMatchers(HttpMethod.GET,
-                                "/api/v1/projects",
                                 "/api/v1/projects/**",
-                                "/api/v1/modjams",
                                 "/api/v1/modjams/**",
                                 "/api/v1/tags",
                                 "/api/v1/files/**",
@@ -285,10 +283,9 @@ public class SecurityConfig {
                                 "/api/v1/auth/change-password",
                                 "/api/v1/auth/credentials"
                         ).authenticated()
-                        .requestMatchers(HttpMethod.POST, "/api/v1/projects", "/api/v1/projects/**", "/api/v1/modjams", "/api/v1/modjams/**").authenticated()
-                        .requestMatchers(HttpMethod.PUT, "/api/v1/projects", "/api/v1/projects/**", "/api/v1/modjams", "/api/v1/modjams/**").authenticated()
-                        .requestMatchers(HttpMethod.DELETE, "/api/v1/projects", "/api/v1/projects/**", "/api/v1/modjams", "/api/v1/modjams/**").authenticated()
-                        .requestMatchers(HttpMethod.PATCH, "/api/v1/modjams", "/api/v1/modjams/**").authenticated()
+                        .requestMatchers(HttpMethod.POST, "/api/v1/projects/**", "/api/v1/modjams/**").authenticated()
+                        .requestMatchers(HttpMethod.PUT, "/api/v1/projects/**", "/api/v1/modjams/**").authenticated()
+                        .requestMatchers(HttpMethod.DELETE, "/api/v1/projects/**", "/api/v1/modjams/**").authenticated()
                         .requestMatchers("/api/**").authenticated()
                         .anyRequest().permitAll()
                 )
