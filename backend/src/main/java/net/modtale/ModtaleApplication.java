481eb11ed18d6b816dc8ccb01118f80e1df2694b
Villagers654 <110007851+Villagers654@users.noreply.github.com>
2026-02-20 19:26:49 -0500

    Managing jams in the dashboard

--- backend/src/main/java/net/modtale/ModtaleApplication.java
+++ backend/src/main/java/net/modtale/ModtaleApplication.java
@@ -2,7 +2,6 @@ package net.modtale;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 public class ModtaleApplication {
