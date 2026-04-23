7bb82d643f8e9c7d514fe12bdd62199b55576888
Villagers654 <110007851+Villagers654@users.noreply.github.com>
2026-04-23 18:30:43 -0400

    I need to start testing before I push

--- backend/src/main/java/net/modtale/controller/resources/OgImageController.java
+++ backend/src/main/java/net/modtale/controller/resources/OgImageController.java
@@ -268,7 +268,7 @@ public class OgImageController {
         g2d.setClip(null);
 
         String type = mod.getClassification() != null ? mod.getClassification() : "PROJECT";
-        int badgeX = x + w - 118;
+        int badgeX = x + w - 30;
         int badgeY = y + 30;
         drawCategoryBadge(g2d, type, badgeX, badgeY);
 
