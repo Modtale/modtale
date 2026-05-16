b47243bac100eb7c631e217a96669aa290cfb2a6
Villagers654 <110007851+Villagers654@users.noreply.github.com>
2026-05-15 20:24:46 -0400

    Just fix auth

--- backend/src/main/java/net/modtale/config/auth/SessionConfig.java
+++ backend/src/main/java/net/modtale/config/auth/SessionConfig.java
@@ -1,36 +1,23 @@
 package net.modtale.config.auth;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.BeanClassLoaderAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.jackson2.SecurityJackson2Modules;
-import org.springframework.session.data.mongo.JacksonMongoSessionConverter;
+import org.springframework.session.data.mongo.JdkMongoSessionConverter;
 import org.springframework.session.data.mongo.config.annotation.web.http.EnableMongoHttpSession;
+
+import java.time.Duration;
 
 @Configuration
 @EnableMongoHttpSession(maxInactiveIntervalInSeconds = 2592000, collectionName = "modtale_sessions") // 30 days
-public class SessionConfig implements BeanClassLoaderAware {
+public class SessionConfig {
 
     private static final Logger logger = LoggerFactory.getLogger(SessionConfig.class);
-    private ClassLoader loader;
-
-    @Override
-    public void setBeanClassLoader(ClassLoader classLoader) {
-        this.loader = classLoader;
-    }
 
     @Bean
-    public JacksonMongoSessionConverter mongoSessionConverter() {
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        objectMapper.registerModules(SecurityJackson2Modules.getModules(this.loader));
-
-        objectMapper.findAndRegisterModules();
-
-        logger.info("Initialized JacksonMongoSessionConverter for JSON-based session storage.");
-        return new JacksonMongoSessionConverter(objectMapper);
+    public JdkMongoSessionConverter mongoSessionConverter() {
+        logger.info("Initialized JdkMongoSessionConverter for robust binary session storage.");
+        return new JdkMongoSessionConverter(Duration.ofDays(30));
     }
 }
