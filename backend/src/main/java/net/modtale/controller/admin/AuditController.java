0d658a8f77abf782e9849eadf6cb85cce19f3454
Villagers654 <110007851+Villagers654@users.noreply.github.com>
2026-05-17 14:16:49 -0400

    Better naming

--- backend/src/main/java/net/modtale/controller/admin/AuditController.java
+++ backend/src/main/java/net/modtale/controller/admin/AuditController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/admin")
-public class AdminAuditController {
+public class AuditController {
 
     @Autowired private AccountService accountService;
     @Autowired private AdminLogRepository adminLogRepository;
