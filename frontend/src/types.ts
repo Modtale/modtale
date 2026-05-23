c59c4640bd61d74de2f930f842b2681d038ef701
Villagers654 <110007851+Villagers654@users.noreply.github.com>
2026-05-23 14:59:14 -0400

    Properly make determining what is a pre release and what isn't backend authoritative

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -76,7 +76,12 @@ export interface GameVersionCatalog {
     releaseVersions: string[];
     preReleaseVersions: string[];
     allVersions: string[];
-    orderedVersions: string[];
+    orderedVersions?: string[];
+    versions?: {
+        version: string;
+        preRelease: boolean;
+        sourceUrl: string;
+    }[];
 }
 
 export interface ScanIssue {
