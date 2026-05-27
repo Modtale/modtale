c5a8e013d100303450f8b609e7703977482af97f
Villagers654 <110007851+Villagers654@users.noreply.github.com>
2026-05-27 19:48:06 -0400

    Automatically select mod version from the manifest.json

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -69,6 +69,7 @@ export interface ManifestDependencySuggestion {
 
 export interface ManifestInspectionResult {
     gameVersion?: string;
+    modVersion?: string;
     suggestions: ManifestDependencySuggestion[];
 }
 
